c2c3044d389c54abf424ebade2e9eac84b9b2495
esentis <esentakos@yahoo.gr>
2020-10-12 00:27:29 +0300

    Adds auth options

---
+++ persistence/controllers/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('express-jwt');
+
+const getTokenFromHeaders = (req) => {
+    const { headers: { authorization } } = req;
+
+    if (authorization && authorization.split(' ')[0] === 'Token') {
+        return authorization.split(' ')[1];
+    }
+    return null;
+};
+
+const auth = {
+    required: jwt({
+        secret: 'secret',
+        userProperty: 'payload',
+        getToken: getTokenFromHeaders,
+        algorithms: ['HS256']
+    }),
+    optional: jwt({
+        secret: 'secret',
+        userProperty: 'payload',
+        getToken: getTokenFromHeaders,
+        credentialsRequired: false,
+        algorithms: ['HS256']
+    }),
+};
+
+module.exports = auth;
