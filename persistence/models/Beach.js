378d629dd2dd66425361f74f63a1e3158cbd3d34
esentis <esentakos@yahoo.gr>
2020-11-02 22:06:57 +0200

    Redesigns model

--- persistence/models/Beach.js
+++ persistence/models/Beach.js
@@ -1,13 +1,11 @@
 var mongoose = require('mongoose');
 
 const beachSchema = new mongoose.Schema({
-    name: { type: String, minlength: [3, 'Minimum characters required 3'], required: [true, 'Name is required'] },
+    name: { type: String, minlength: [2, 'Minimum characters required 2'], required: [true, 'Name is required'] },
     description: { type: String, minlength: [3, 'Minimum characters required 25'], required: [true, 'Description is required'] },
-    img: { type: String, required: [true, 'Image is required'] },
+    images: { type: [String], required: [true, 'Images are required'] },
     latitude: { type: Number, required: [true, 'Latitude is required'] },
-    longtitude: { type: Number, required: [true, 'Longtitude is required'] },
-    voteCount: { type: Number },
-    rating: { type: Number },
+    longitude: { type: Number, required: [true, 'Longtitude is required'] },
 });
 
 beachSchema.methods.toDto = function () {
@@ -15,10 +13,8 @@ beachSchema.methods.toDto = function () {
         name: this.name,
         description: this.description,
         latitude: this.latitude,
-        longtitude: this.longtitude,
-        voteCount: this.voteCount,
-        rating: this.rating,
-        img: this.img
+        longitude: this.longitude,
+        images: this.images
     }
 }
 const Beach = mongoose.model('Beach', beachSchema);
