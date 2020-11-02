978629e9b994176ad8398251df9ac84e6e74649f
esentis <esentakos@yahoo.gr>
2020-11-02 22:05:58 +0200

    Removes user authentication

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var express = require('express');
-// var ads = require('./controllers/adController.js');
-var users = require('./persistence/controllers/user_controller.js');
+
 var beaches = require('./persistence/controllers/beach_controller.js')
 var bodyParser = require('body-parser');
 require('./db_connection.js');
-require('./services/passport_config')
+
 const app = express();
 
 // create application/json parser
 var jsonParser = bodyParser.json()
 
 app.use('/beaches', jsonParser, beaches);
-app.use('/user', jsonParser, users);
+
 
 app.listen(3000, function () {
     console.log("Express server listening on port ", 3000);
