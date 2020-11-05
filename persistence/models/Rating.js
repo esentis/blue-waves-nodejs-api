b078e4735df99bc3f9cecb480e30b6b6650efa2b
esentis <esentakos@yahoo.gr>
2020-11-05 21:39:14 +0200

    Adds rating model

---
+++ persistence/models/Rating.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+const ratingSchema = new mongoose.Schema({
+    beachId: { type: String, required: [true, 'Beach ID is required'] },
+    userId: { type: String, required: [true, 'User ID is required'] },
+    rating: { type: Number, required: [true, 'Rating is required'] },
+});
+
+ratingSchema.methods.toDto = function () {
+    return {
+        beachId: this.beachId,
+        userId: this.userId,
+        rating: this.rating
+    }
+}
+const Rating = mongoose.model('Rating', ratingSchema);
+
+module.exports = Rating;
