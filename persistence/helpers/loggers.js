1b1590f81a35038cb1ca05d33b6e0abddbd10206
esentis <esentakos@yahoo.gr>
2020-11-07 23:46:02 +0200

    Setups loggers

---
+++ persistence/helpers/loggers.js
@@ -0,0 +1,33 @@
+var winston = require('winston');
+
+
+const myCustomLevels = {
+    levels: {
+        error: 0,
+        warn: 1,
+        info: 2,
+        success: 2
+    },
+};
+
+winston.addColors({
+    error: 'red bold',
+    warn: 'yellow bold',
+    info: 'cyan bold',
+    success: 'green bold'
+});
+
+const logger = winston.createLogger({
+    levels: myCustomLevels.levels,
+    format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss' }),
+        winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logfile.log', level: 'error' },
+        )]
+});
+
+module.exports = logger;
