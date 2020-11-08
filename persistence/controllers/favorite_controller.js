0eb4aafcb5b79eb0832c8dac4d2c77ec33e884b8
esentis <esentakos@yahoo.gr>
2020-11-08 22:02:16 +0200

    Adds favorite functionality
    If beach is already favorited, we just remove it.

--- persistence/controllers/favorite_controller.js
+++ persistence/controllers/favorite_controller.js
@@ -9,36 +9,50 @@ router.post('/add', async function (req, res) {
 
     var favorite = await Favorite.findOne({ beachId: req.body.beachId, userId: req.body.userId }).exec();
     if (favorite != null) {
-        logger.error(`User ${req.body.userId} has already favorited beach ${req.body.beachId}`);
-        return res.status(400).json({ success: false, msg: 'You have already favorited the beach!' });
+        await Favorite.deleteOne({ beachId: req.body.beachId, userId: req.body.userId })
+            .exec(function (err, favorites) {
+                if (err) {
+                    logger.error(`Something went wrong ! ${err}`);
+                }
+                else if (!favorites) {
+                    logger.error(`User has no favorites.`);
+                } else {
+                    logger.warn(`Beach has been removed from favorites list.`);
+                    return res.status(204).json({ success: false, msg: 'Beach has been removed from favorites.' });
+                }
+
+            });
+
+    } else {
+        const newFavorite = new Favorite({
+            beachId: req.body.beachId,
+            userId: req.body.userId
+        })
+
+        await newFavorite.save(function (err, newAdd) {
+            if (err) {
+                logger.error(`Something went wrong ! ${err}`)
+                return res.status(400).json({ success: false, msg: err });
+            } else {
+                logger.success(`Beach with ID ${req.body.beachId} has been favorited`);
+                res.status(201).json({ success: true, createdAt: (req.get('host') + req.baseUrl + '/' + newAdd._id) });
+            }
+        });
     }
 
-    const newFavorite = new Favorite({
-        beachId: req.body.beachId,
-        userId: req.body.userId
-    })
-
-    await newFavorite.save(function (err, newAdd) {
-        if (err) {
-            logger.error(`Something went wrong ! ${err}`)
-            return res.status(400).json({ success: false, msg: err });
-        } else {
-            logger.success(`Beach with ID ${req.body.beachId} has been favorited`);
-            res.status(201).json({ success: true, createdAt: (req.get('host') + req.baseUrl + '/' + newAdd._id) });
-        }
-    });
 });
 
 // Searches for a specific user's favorited beaches.
 router.post('/search', async function (req, res) {
     var userId = req.body.userId;
+    var beachId = req.body.beachId;
     // Restricts search term's length to avoid unnecessary document reads.
     if (userId.length <= 4) {
         logger.error(`At least 4 characters are needed to perform search`);
         res.status(404).json({ success: false, msg: "At least 4 characters are needed to perform search" });
     } else {
-        var favorites = await Favorite.find({ 'userId': userId }).exec();
-        logger.info(`Found ${favorites.length} favorited beaches.`);
+        var favorites = await Favorite.find({ userId: userId, beachId: beachId }).exec();
+        logger.warn(`${favorites.length == 0 ? 'Beach is not favorited by the user.' : 'Beach is favorited.'}`);
         res.json({
             results: favorites
         });
