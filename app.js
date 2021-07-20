7d6d9b2247f31e0cb61026e52740db2fbe3ad2d0
George Leonidis <esentakos@yahoo.gr>
2021-07-21 00:50:43 +0300

    🔧 Adds heroku config port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/ratings", ratings);
 app.use("/favorites", favorites);
 app.use("/users", users);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Express server listening on port ", process.env.PORT || 3000);
 });
 
