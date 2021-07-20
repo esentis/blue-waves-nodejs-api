ba59dc51636108f13f2a12fe87fa59fbb05e19fd
George Leonidis <esentakos@yahoo.gr>
2021-07-20 20:18:44 +0300

    ✨ Adds checks for user existence

--- persistence/controllers/user_controller.js
+++ persistence/controllers/user_controller.js
@@ -1,76 +1,97 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var User = require('../models/User.js');
-var Rating = require('../models/Rating.js');
-var Favorite = require('../models/Favorite.js');
+var User = require("../models/User.js");
+var Rating = require("../models/Rating.js");
+var Favorite = require("../models/Favorite.js");
 
-const logger = require('../helpers/loggers.js');
+const logger = require("../helpers/loggers.js");
 
-router.post('/add', async function (req, res) {
+router.post("/add", async function (req, res) {
+  const newUser = new User({
+    username: req.body.username,
+    karma: 0,
+    role: "user",
+    id: req.body.id,
+    joinDate: req.body.joinDate,
+  });
 
-    const newUser = new User({
-        username: req.body.username,
-        karma: 0,
-        role: 'user',
-        id: req.body.id,
-        joinDate: req.body.joinDate,
-    })
-
-    await newUser.save(function (err, newAdd) {
-        if (err) {
-            logger.error(`Something went wrong ! ${err}`);
-            return res.status(400).json({ success: false, msg: err });
+  await User.findOne(
+    { username: req.body.username },
+    async function (err, user) {
+      if (err) {
+        logger.error(`Something went wrong ! ${err}`);
+        return res.status(400).json({ success: false, msg: err });
+      } else {
+        if (user != null) {
+          logger.error("Username already exists");
+          return res
+            .status(400)
+            .json({ success: false, msg: "Username already exists!" });
         } else {
-            logger.success(`New user created at ${req.get('host') + req.baseUrl + '/' + newAdd._id}`);
-            res.status(201).json({ success: true, createdAt: (req.get('host') + req.baseUrl + '/' + newAdd._id) });
+          await newUser.save(function (err, newAdd) {
+            if (err) {
+              logger.error(`Something went wrong ! ${err}`);
+              return res.status(400).json({ success: false, msg: err });
+            } else {
+              logger.success(
+                `New user created at ${
+                  req.get("host") + req.baseUrl + "/" + newAdd._id
+                }`
+              );
+              res.status(201).json({
+                success: true,
+                createdAt: req.get("host") + req.baseUrl + "/" + newAdd._id,
+              });
+            }
+          });
         }
-    });
+      }
+    }
+  );
 });
 
-router.post('/delete/', async function (req, res) {
-    logger.info(`Request to delete user with ID: ${req.body.id}`);
-    await Rating.deleteMany({ userId: req.body.id })
-        .exec(function (err, ratings) {
-            if (err) {
-                logger.error(`Something went wrong ! ${err}`);
-            }
-            else if (!ratings) {
-                logger.error(`User has no ratings.`);
-            } else {
-                logger.warn(`${ratings.deletedCount} ratings deleted.`);
+router.post("/delete/", async function (req, res) {
+  logger.info(`Request to delete user with ID: ${req.body.id}`);
+  await Rating.deleteMany({ userId: req.body.id }).exec(function (
+    err,
+    ratings
+  ) {
+    if (err) {
+      logger.error(`Something went wrong ! ${err}`);
+    } else if (!ratings) {
+      logger.error(`User has no ratings.`);
+    } else {
+      logger.warn(`${ratings.deletedCount} ratings deleted.`);
+    }
+  });
 
-            }
+  await Favorite.deleteMany({ userId: req.body.id }).exec(function (
+    err,
+    favorites
+  ) {
+    if (err) {
+      logger.error(`Something went wrong ! ${err}`);
+    } else if (!favorites) {
+      logger.error(`User has no favorites.`);
+    } else {
+      logger.warn(`${favorites.deletedCount} favorites deleted.`);
+    }
+  });
 
-        });
-
-    await Favorite.deleteMany({ userId: req.body.id })
-        .exec(function (err, favorites) {
-            if (err) {
-                logger.error(`Something went wrong ! ${err}`);
-            }
-            else if (!favorites) {
-                logger.error(`User has no favorites.`);
-            } else {
-                logger.warn(`${favorites.deletedCount} favorites deleted.`);
-            }
-
-        });
-
-    await User.findOneAndDelete({ id: req.body.id })
-        .exec(function (err, user) {
-            if (err) {
-                logger.error(`Something went wrong ! ${err}`);
-                res.status(400).json({ success: false, msg: err });
-            }
-            else if (!user) {
-                logger.error(`User not found in database.`);
-                res.status(404).json({ success: false, msg: 'User not found in database.' });
-            } else {
-                logger.success(`User has been successfully deleted.`);
-                res.json({ success: true, msg: 'User successfuly deleted.' });
-            }
-
-        });
+  await User.findOneAndDelete({ id: req.body.id }).exec(function (err, user) {
+    if (err) {
+      logger.error(`Something went wrong ! ${err}`);
+      res.status(400).json({ success: false, msg: err });
+    } else if (!user) {
+      logger.error(`User not found in database.`);
+      res
+        .status(404)
+        .json({ success: false, msg: "User not found in database." });
+    } else {
+      logger.success(`User has been successfully deleted.`);
+      res.json({ success: true, msg: "User successfuly deleted." });
+    }
+  });
 });
 
 module.exports = router;
