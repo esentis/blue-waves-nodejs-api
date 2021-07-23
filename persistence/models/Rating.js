a47e7b4bf04ea512d7df26133b00ee7ed4f4393c
George Leonidis <esentakos@yahoo.gr>
2021-07-23 18:17:38 +0300

    ✨ Adds rating validation

--- persistence/models/Rating.js
+++ persistence/models/Rating.js
@@ -3,7 +3,13 @@ var mongoose = require("mongoose");
 const ratingSchema = new mongoose.Schema({
   beachId: { type: String, required: [true, "Beach ID is required"] },
   userId: { type: String, required: [true, "User ID is required"] },
-  rating: { type: Number, required: [true, "Rating is required"] },
+  rating: {
+    type: Number,
+    required: [true, "Rating is required"],
+    min: 0,
+    max: 10,
+  },
+  review: String,
 });
 
 ratingSchema.methods.toDto = function () {
@@ -11,6 +17,7 @@ ratingSchema.methods.toDto = function () {
     beachId: this.beachId,
     userId: this.userId,
     rating: this.rating,
+    review: this.review,
   };
 };
 const Rating = mongoose.model("Rating", ratingSchema);
